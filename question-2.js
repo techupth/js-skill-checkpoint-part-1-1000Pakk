99d8fea0ca533f6966ad7b82506df3f50fcb8f98
Panpak Apesariyayo <ingpanpak@gmail.com>
2024-05-22 15:47:29 +0700

    JsSkillCheckpoint1

--- question-2.js
+++ question-2.js
@@ -4,3 +4,13 @@ const inventory = [
   { name: "Banana", price: 10, quantity: 50 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+inventory[0].quantity = 200;
+const orangeObj = { name: "Orange", price: 20, quantity: 300 };
+inventory.push(orangeObj);
+
+const totalPrice = inventory.reduce(
+  (acc, curr) => (acc += curr.price * curr.quantity),
+  0
+);
+
+console.log(`มูลค่ารวมของจำนวนสินค้าทั้งหมดในสต็อก ${totalPrice} บาท`);
--- question-4.js
+++ question-4.js
@@ -5,3 +5,21 @@ const inventory = [
   { name: "Orange", price: 30, quantity: 60 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+let lowestQuantityResult = function (inventory) {
+  let lowestQuantity = inventory.reduce(
+    (acc, curr) => (curr.quantity < acc ? (acc = curr.quantity) : acc),
+    Infinity
+  );
+  let lowestQuantity_Name = inventory.reduce((acc, curr) => {
+    if (curr.quantity < acc.quantity) {
+      return (acc = curr);
+    } else {
+      return acc;
+    }
+  });
+  let nameFruit = lowestQuantity_Name.name;
+  console.log(nameFruit);
+  return `สินค้าที่มีจำนวนต่ำที่สุดในคลังสินค้าคือ ${nameFruit} ซึ่งมี ${lowestQuantity} ชิ้น`;
+};
+
+console.log(lowestQuantityResult(inventory));
